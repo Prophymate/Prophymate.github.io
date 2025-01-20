e33ec8ae4cab060e645d9d7a2f3cd289252ab1e5
Prophymate <92168391+Prophymate@users.noreply.github.com>
2025-01-20 08:56:19 +0100

    up

---
+++ src/CMakeNinja.cpp
@@ -0,0 +1,17 @@
+﻿//#include "../include/CMakeNinja"
+//
+//using namespace std;
+//
+//int main() {
+//    sf::RenderWindow window(sf::VideoMode(800, 600), "SFML Window");
+//    while (window.isOpen()) {
+//        sf::Event event;
+//        while (window.pollEvent(event)) {
+//            if (event.type == sf::Event::Closed)
+//                window.close();
+//        }
+//        window.clear();
+//        window.display();
+//    }
+//    return 0;
+//}
